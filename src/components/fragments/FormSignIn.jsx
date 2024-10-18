224888c2aa7f652fec3e350ddbc3c0a836c502c2
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2024-10-18 08:35:35 +0700

    menambahkan props

--- src/components/fragments/FormSignIn.jsx
+++ src/components/fragments/FormSignIn.jsx
@@ -1,24 +1,33 @@
-import React from "react";
-import LabeledInput from "../Elements/LabeledInput";
-import CheckBox from "../Elements/CheckBox";
 import Button from "../Elements/Button";
+import CheckBox from "../Elements/CheckBox";
+import LabeledInput from "../Elements/LabeledInput";
 
 const FormSignIn = () => {
   return (
-    <div className="mt-16">
-      <form action="">
-        <div className="mb-6">
-          <LabeledInput />
-        </div>
-        <div className="mb-6">
-          <LabeledInput />
-        </div>
-        <div className="mb-3">
-          <CheckBox />
-        </div>
-        <Button />
-      </form>
-    </div>
+    <form action="">
+      <div className="mb-6">
+        <LabeledInput
+          label="Email address"
+          type="email"
+          placeholder="hello@example.com"
+          name="email"
+        />
+      </div>
+      <div className="mb-6">
+        <LabeledInput
+          label="Password"
+          type="password"
+          placeholder="*************"
+          name="password"
+        />
+      </div>
+      <div className="mb-3">
+        <CheckBox label="Keep me signed in" name="status" />
+      </div>
+      <Button variant="bg-primary w-full text-white" type="submit">
+	      Login
+      </Button>
+    </form>
   );
 };
 
