d58c31da4b2fd1ffdef520d0103b54e0d3788064
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2024-10-29 21:33:18 +0700

    update

--- src/pages/balance.jsx
+++ src/pages/balance.jsx
@@ -1,5 +1,5 @@
 import Card from "../components/Elements/Card";
-import MainLayout from "../components/Layouts/MainLayout";
+import MainLayout from "../components/layouts/MainLayout";
 
 const BalancePage = () => {
   return (
