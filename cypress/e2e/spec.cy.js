9805130271014ee417c63fb5e02d5cc3a06a3376
ajiekusumadhany <ajieikruskusumadhany@gmail.com>
2025-01-12 21:34:53 +0700

    update cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 // Skenario
 // 1. User berhasil login dengan kredensial yang valid.
 // 2. Setelah login, user diarahkan ke halaman dashboard (overview).
-describe("User  mengakses halaman dashboard (overview)", () => {
+describe("User   mengakses halaman dashboard (overview)", () => {
   it("should allow user to log in and access the dashboard", () => {
     // Langkah 1: Buka halaman login
     cy.viewport(1280, 800);
@@ -27,32 +27,20 @@ describe("User  mengakses halaman dashboard (overview)", () => {
     cy.get("button").contains("Login").click();
 
     // Langkah 5: Verifikasi URL dashboard atau overview
-    cy.url().should("include", "/");
+    cy.url().should("eq", "https://admin-ui-ajie.vercel.app/");
 
-    // Langkah 6: Verifikasi navigasi dan header setelah login
-    cy.get("nav").should("be.visible");
+    // Langkah 6: Verifikasi elemen header
     cy.get("header").should("be.visible");
+    cy.get("header").should("be.visible");
+
+    // Langkah 7: Verifikasi elemen nav
+    cy.get("nav").should("be.visible");
+
+    // Langkah 8: Verifikasi elemen main
+    cy.get("main").should("be.visible");
+
+
+    cy.wait(2000); // Tunggu 2 detik
+
   });
-
-  // it("should not allow user to log in with invalid credentials", () => {
-  //   cy.visit("http://localhost:5173/");
-
-  //   cy.url().should("include", "/login");
-
-  //   cy.get("input#email")
-  //     .should("be.visible")
-  //     .should("have.attr", "placeholder", "hello@example.com")
-  //     .type("hello@example.com")
-  //     .should("have.value", "hello@example.com");
-
-  //   cy.get("input#password")
-  //     .should("be.visible")
-  //     .should("have.attr", "placeholder", "*************")
-  //     .type("123")
-  //     .should("have.value", "123");
-
-  //   cy.get("button").contains("Login").click();
-
-  //   cy.get("div").contains("Wrong Password");
-  // });
 });
